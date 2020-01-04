776c2a4db0ba2ad81fbf80cb4b3bb8c70cecf5bb
Patric Lacouth <lacouth@gmail.com>
2020-01-04 18:21:12 -0300

    Add api.js

---
+++ js/api.js
@@ -0,0 +1,38 @@
+let div_images = document.querySelector('#images')
+let input = document.querySelector('#qbName')
+
+let json = []
+
+input.addEventListener('input', () => {
+    console.log(input.value)
+    renderQBS(json,input.value.toLowerCase())
+})
+
+
+fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players?key=9bb929f203854586a80ed06ce03c813a')
+.then(res => res.json())
+.then(j => {
+            json = j
+            renderQBS(json)
+    })
+
+
+function renderQBS(json,filter = ""){
+    div_images.innerHTML = ""
+    for(let player of json){
+        if(player.Active && player.Position === "QB" && player.ShortName.toLowerCase().includes(filter)){
+            let img = `<div class="col-sm-2">
+                            <div class="card"> 
+                                <img src=${player.PhotoUrl} class="card-img-top"/>
+                                <div class="card-body">
+                                    <p class="card-text" >${player.ShortName} </p>
+                                    <p class="card-text" >${player.Team}</p>
+                                    <p hidden="true">${player.PlayerID}</p>
+                                </div>
+                            </div>
+                        </div>`
+            div_images.insertAdjacentHTML('beforeend',img)
+        }
+    }
+}
+
